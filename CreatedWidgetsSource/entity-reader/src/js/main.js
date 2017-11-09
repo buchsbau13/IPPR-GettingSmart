d59f7088bb5e291a4d43ab7e81c25248ef36aedf
januschm13 <michi_janusch@chello.at>
2017-11-09 15:41:12 +0100

    Added possibility to choose attribute type to entity reader widget

--- CreatedWidgetsSource/entity-reader/src/js/main.js
+++ CreatedWidgetsSource/entity-reader/src/js/main.js
@@ -8,11 +8,20 @@
         var entity = JSON.parse(entityString);
         var attribute = MashupPlatform.prefs.get('attribute');
         var unit = MashupPlatform.prefs.get('unit');
+        var valueType = MashupPlatform.prefs.get('valueType');
 
         if (attribute) {
             var value = entity[attribute];
             var jsonObject = {};
-            jsonObject.value = parseInt(value);
+
+            if (valueType == "int") {
+                jsonObject.value = parseInt(value);
+            } else if (valueType == "float") {
+                jsonObject.value = parseFloat(value);
+            } else {
+                jsonObject.value = value;
+            }
+
             jsonObject.unit = unit;
         } else {
             return;
