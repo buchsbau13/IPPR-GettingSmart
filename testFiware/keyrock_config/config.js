438ebd2d2d0f01ebce42d7f25c3d5c5f7961edf5
januschm13 <michi_janusch@chello.at>
2018-05-31 10:17:57 +0200

    Added FIWARE security configuration

---
+++ testFiware/keyrock_config/config.js
@@ -0,0 +1,92 @@
+var config = {};
+
+config.host = 'http://localhost:5000';
+config.port = 5000
+
+// HTTPS enable
+config.https = {
+    enabled: false,
+    cert_file: 'certs/dummy.crt',
+    key_file: 'certs/dummy.key',
+    //cert_file: 'certs/live/graziot.info.tm/fullchain.pem',
+    //key_file: 'certs/live/graziot.info.tm/privkey.pem',
+    port: 5005
+};
+
+// Config email list type to use domain filtering
+config.email_list_type = null   // whitelist or blacklist 
+
+// Secret for user sessions in web
+config.session = {
+    secret: 'scvbvWFz54hfs',       // Must be changed
+    expires: 60 * 60 * 1000     // 1 hour
+}
+
+// Key to encrypt user passwords
+config.password_encryption = {
+	key: 'rjNEsz647tzewu'		// Must be changed
+}
+
+// Config oauth2 parameters
+config.oauth2 = {
+    authorization_code_lifetime: 5 * 60,            // Five minutes
+    access_token_lifetime: 60 * 60,                 // One hour
+    refresh_token_lifetime: 60 * 60 * 24 * 14       // Two weeks
+}
+
+// Config api parameters
+config.api = {
+    token_lifetime: 60 * 60     // One hour
+}
+
+// Enable authzforce
+config.authzforce = {
+	enabled: true,
+	host: 'authzforce',
+	port: 8080
+}
+
+var database_host = (process.env.DATABASE_HOST) ? process.env.DATABASE_HOST : 'localhost'
+
+// Database info
+config.database = {
+    host: database_host,         // default: 'localhost' 
+    password: 'idm',             // default: 'idm'
+    username: 'root',            // default: 'root'
+    database: 'idm',             // default: 'idm'
+    dialect: 'mysql'             // default: 'mysql'
+};
+
+// External user authentication
+config.external_auth = {
+    enabled: false,
+    authentication_driver: 'custom_authentication_driver',
+    database: {
+        host: 'localhost',
+        database: 'db_name',
+        username: 'db_user',
+        password: 'db_pass',
+        user_table: 'user',
+        dialect: 'mysql'
+    }
+}
+
+// Email configuration
+config.mail = {
+    host: 'smtp.office365.com',
+    port: 587,
+    auth: {
+      user: 'graziot@outlook.com',
+      pass: 'IPPR_aim16'      
+    },
+    from: 'graziot@outlook.com'
+}
+
+
+// Config themes
+config.site = {
+    title: 'Identity Manager',
+    theme: 'default'
+};
+
+module.exports = config;
---
+++ testFiware/pepOrion_config/config.js
@@ -0,0 +1,57 @@
+var config = {};
+
+// Used only if https is disabled
+config.pep_port = 1027;
+
+// Set this var to undefined if you don't want the server to listen on HTTPS
+config.https = {
+    enabled: false,
+    cert_file: 'cert/cert.crt',
+    key_file: 'cert/key.key',
+    port: 443
+};
+
+config.idm = {
+	host: 'keyrock',
+	port: 5000,
+	ssl: false
+}
+
+config.app = {
+	host: 'orion',
+	port: '1026',
+	ssl: false // Use true if the app server listens in https
+}
+
+
+// Credentials obtained when registering PEP Proxy in app_id in Account Portal
+config.pep = {
+	app_id: '0e70db25-b5ff-40e3-a8d9-5ae0f06819a6',
+	username: 'pep_proxy_969b0f7f-6fc9-4e8e-a731-c83f3daef174',
+	password: 'pep_proxy_959aff11-37b8-4852-9fe3-f1e0b6de094a',
+	trusted_apps : []
+}
+
+// in seconds
+config.cache_time = 300;
+
+// if enabled PEP checks permissions with AuthZForce GE. 
+// only compatible with oauth2 tokens engine
+//
+// you can use custom policy checks by including programatic scripts 
+// in policies folder. An script template is included there
+config.azf = {
+	enabled: true,
+	protocol: 'http',
+    host: 'authzforce',
+    port: 8080,
+    custom_policy: undefined // use undefined to default policy checks (HTTP verb + path).
+};
+
+// list of paths that will not check authentication/authorization
+// example: ['/public/*', '/static/css/']
+config.public_paths = [];
+
+config.magic_key = undefined;
+
+module.exports = config;
