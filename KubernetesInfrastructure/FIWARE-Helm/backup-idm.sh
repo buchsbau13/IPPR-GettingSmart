cdac4702191f14fda24f1225c79a35fe3135c247
fiware-austria <peter.salhofer@fh-joanneum.at>
2019-03-21 19:22:50 +0100

    further improvements and fixes

--- KubernetesInfrastructure/FIWARE-Helm/backup-idm.sh
+++ KubernetesInfrastructure/FIWARE-Helm/backup-idm.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kubectl exec -t mysql-0 -n fiware-graziot -- mysqldump --password=idm idm > ./idm-backup/idm.sql
+kubectl exec -t mysql-0 -n fiware-graziot -- mysqldump --complete-insert --skip-add-locks --skip-add-drop-table --no-create-db --no-create-info --ignore-table=idm.SequelizeMeta  --password=idm idm > ./idm-backup/idm.sql
